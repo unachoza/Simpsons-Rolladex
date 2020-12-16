28505df86444a4cab64d44f22e8521457e0a3578
arianna choza <unachoza@gmail.com>
2020-12-15 20:00:20 -0800

    hitting api

--- src/Components/CardList/CardList.component.js
+++ src/Components/CardList/CardList.component.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '../Card/Card.component';
+import FetchSimpsons from '../../api/Fetch';
 
-const CardList = () => {
-  return (
-    <div>
-      CardList Container
-      <Card />
-    </div>
-  );
-};
+class CardList extends Component {
+  componentDidMount() {
+    FetchSimpsons();
+  }
+
+  render() {
+    return (
+      <div>
+        CardList Container
+        <Card />
+      </div>
+    );
+  }
+}
 
 export default CardList;
