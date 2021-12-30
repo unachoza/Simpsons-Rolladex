c0a6d1d24512201891c56434723fd5763f1e8c07
arianna choza <unachoza@gmail.com>
2021-12-30 16:10:51 -0500

    card flips with quote on back; card list is off center

--- src/Components/Card/Card.component.js
+++ src/Components/Card/Card.component.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import './Card.styles.css';
+import React from "react";
+import "./Card.styles.css";
 
-const Card = ({ image, id, character }) => {
+const Card = ({ image, id, character, quote }) => {
   return (
-    <div key={id} className="card">
-      <img src={image} alt="simpsons character" className="card-img" />
-      <br />
-      <div className="card-text"> {character}</div>
-    </div>
+    <label>
+      <input type="checkbox" />
+      <div class="flip-card">
+        <div key={id} className="card front">
+          <img src={image} alt="simpsons character" className="card-img" />
+          <br />
+          <div className="card-text"> {character}</div>
+        </div>
+        <div class="card back">
+          <div className="card-quote"> "{quote}"</div>
+        </div>
+      </div>
+    </label>
   );
 };
 
--- src/Components/CardList/CardList.component.js
+++ src/Components/CardList/CardList.component.js
@@ -4,11 +4,10 @@ import './CardList.styles.css';
 import Loading from '../Loading/Loading.component';
 
 const CardList = (filteredCharacters) => {
-  console.log(filteredCharacters.simpsons);
   return (
     <div className="card-list">
       {filteredCharacters.simpsons.length > 0 ? (
-        filteredCharacters.simpsons.map((simp) => <Card image={simp.image} id={simp.id} character={simp.character} />)
+        filteredCharacters.simpsons.map((simp, i) => <Card image={simp.image} key={i} id={simp.id} character={simp.character} quote={simp.quote} />)
       ) : (
         <Loading />
       )}
