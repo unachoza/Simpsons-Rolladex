aad55258fdcff2db62e686ec72fb0d7d9c392d39
arianna choza <unachoza@gmail.com>
2022-01-22 18:49:45 -0500

    search component is back with different styles

--- src/Components/CardList/CardList.component.js
+++ src/Components/CardList/CardList.component.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import Card from '../Card/Card.component';
-import './CardList.styles.css';
+import React from "react";
+import Card from "../Card/Card.component";
+import "./CardList.styles.css";
 
 const CardList = (filteredCharacters) => {
   return (
     <div className="card-list">
-       { filteredCharacters.simpsons.map((simp, i) => <Card image={simp.image} key={i} id={simp.id} character={simp.character} quote={simp.quote} />)}
+      {filteredCharacters.simpsons.map((simp, i) => (
+        <Card image={simp.image} key={i} id={simp.id} character={simp.character} quote={simp.quote} />
+      ))}
     </div>
   );
 };
